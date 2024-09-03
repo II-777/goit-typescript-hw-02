c34fd990dc5e05ca7e625d843f0270a342508b5c
II-777 <45958058+II-777@users.noreply.github.com>
2024-09-03 00:36:34 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+---
+title: README
+date: 2024-09-03 00:34
+author: II-777
+tags: React TypeScript Vite 
+---
+
+# goit-typescript-hw-02
---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import './App.module.css'
+
+function App() {
+  return (
+    <>
+      <h1>goit-typescript-hw-02</h1>
+    </>
+  )
+}
+
+export default App
